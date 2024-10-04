bb37d10875d4ab03b59c84324cd7332540a6c0a4
Jelly-man <kent.yates4367@gmail.com>
2024-10-04 19:46:15 +0100

    na

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
+
 import { logo } from "../Utils/constants";
+import { SearchBar } from "./SearchBar";
 
 const Navbar = () => (
   <Stack
@@ -18,6 +20,7 @@ const Navbar = () => (
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
+    <SearchBar />
   </Stack>
 );
 
